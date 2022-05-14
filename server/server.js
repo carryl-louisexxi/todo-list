c9ed877a22457d9b1676905ae7c89b10c1921646
Carryl21 <cjdelatorre1@up.edu.ph>
2022-05-15 07:22:14 +0800

    build client folder

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const app = express()
 // set up port 
 const PORT = process.env.PORT || 8080 
 
+app.use(express.static("public"))
 app.use(cors({ origin: '*' }))  
 app.use(express.json({limit: '1mb'}))
 app.use( express.urlencoded({ extended: true }))
